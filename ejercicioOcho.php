00de97616e3131cda72d1d92698796f15ee95144
NebulosaCosmica <ariel257cuevas@yahoo.com.ar>
2016-08-23 19:52:07 -0300

    Ejercicio ocho arrays asociativos revisarlos

---
+++ ejercicioOcho.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+	<title>Cuevas</title>
+</head>
+<body>
+<?php 
+
+//$v[1]=90;
+//$v[30]=7;
+//$v['e']= 99;
+//$v['hola']= 'mundo';
+
+  $v = array (1=>90,30 => 7, 'e'=> 99, 'hola'=>'mundo' );
+
+foreach ($v as $dato)
+{
+	//echo "$dato <br>";
+	Var_Dump($dato);
+	echo "<br>";
+
+	//ver var_dump y foreach
+}
+
+
+
+?>
+</body>
+</html>
