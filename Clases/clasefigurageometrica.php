a9cd12d8a4bf4bf8bb583d453b43d6999931673b
NebulosaCosmica <ariel257cuevas@yahoo.com.ar>
2016-08-23 21:23:46 -0300

    objetos duros

---
+++ Clases/clasefigurageometrica.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<?php 
+/**
+* 
+*/
+class FiguraGeometrica
+{
+	protected _color;
+	proteted 
+
+	function __construct(argument)
+	{
+		
+	}
+
+
+}
+
+ ?>
+
+</body>
+</html>
